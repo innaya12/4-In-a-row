c415fd7e244f30d49cc4d505bd06ac170c9bb5b9
unknown <yakubov.inna2@gmail.com>
2019-12-26 19:05:56 +0200

    before creating board

--- connect4/src/App.js
+++ connect4/src/App.js
@@ -7,39 +7,36 @@ import {
   Link
 } from "react-router-dom";
 import GameEntry from './component/gameEntry';
-import OnePlayer from './component/onePlayer';
-import TwoPlayers from './component/twoPlayers'
+import OnePlayer from './component/player';
 import Play from './component/play'
-// const game = require('./classes/game');
-
-// const Board = require('./classes/board');
+const game = require('./classes/game');
+const Board = require('./classes/board');
 
 function App() {
-  game.initBoard(4, 4);
-  game.move(0, 'pink');
-  game.move(0, 'pink');
-  game.move(0, 'blue');
-  game.move(0, 'pink');
-  game.move(1, 'blue');
-  game.move(1, 'pink');
-  game.move(1, 'pink');
-  game.move(1, 'blue');
-  game.move(2, 'blue');
-  game.move(2, 'pink');
-  game.move(2, 'pink');
-  game.move(2, 'blue');
-  game.move(3, 'pink');
-  game.move(3, 'blue');
-  game.move(3, 'pink');
-  game.move(3, 'pink');
+  // game.initBoard(4, 4);
+  // game.move(0, 'pink');
+  // game.move(0, 'pink');
+  // game.move(0, 'blue');
+  // game.move(0, 'pink');
+  // game.move(1, 'blue');
+  // game.move(1, 'pink');
+  // game.move(1, 'pink');
+  // game.move(1, 'blue');
+  // game.move(2, 'blue');
+  // game.move(2, 'pink');
+  // game.move(2, 'pink');
+  // game.move(2, 'blue');
+  // game.move(3, 'pink');
+  // game.move(3, 'blue');
+  // game.move(3, 'pink');
+  // game.move(3, 'pink');
   return (
     <Router>
       <div className="App">
         <Switch>
-          {/* <Route path={"/oneplayer"} component={OnePlayer}/>
-          <Route path={"/twoplayers"} component={TwoPlayers}/>
+          <Route path={"/Player"} component={OnePlayer}/>
           <Route path={"/play"} component={Play}/>
-          <Route path={"/"} component={GameEntry}/> */}
+          <Route path={"/"} component={GameEntry}/>
         </Switch>
       </div>
     </Router>
--- connect4/src/component/play.jsx
+++ connect4/src/component/play.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
+const game = require('../classes/game');
 
 class Play extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+        };
+        this.getSettings();
+    }
+    
+    getSettings(){
+        // console.log("1", game.getPlayer1());
+        // console.log("2", game.getPlayer2());
+        console.log("3", game.getCurrentPlayer());
+        // console.log("4", game.getBoard());
+        let board = 4;
+    }
+
+
     render(){
         return(
             <div>
