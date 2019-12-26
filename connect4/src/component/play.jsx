0e715c44b7d0cefa2d2f28d565ad236386afb340
unknown <yakubov.inna2@gmail.com>
2019-12-26 14:00:41 +0200

    second commit

---
+++ connect4/src/component/play.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Play extends React.Component{
+    render(){
+        return(
+            <div>
+                <h6>component game</h6>
+            </div>
+        )
+
+    }
+}
+
+export default Play;
